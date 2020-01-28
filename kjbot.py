7dcd9968a866fe78ef208b0fa79e43cb5895b414
furkan <furkanaldemir@gmail.com>
2020-01-28 13:26:56 +0200

    Fix structure

--- kjbot.py
+++ kjbot.py
@@ -41,112 +41,26 @@ def display_status(update, context, balance_before = ['', '', '']):
     except:
         context.bot.send_message(chat_id=config.chat_id, text=msg, parse_mode='markdown')
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def start_callback(update, context):
-    if correct_chat(update.message.chat_id):
-        msg = "Hi dear master, "
-        msg += str(update.message.from_user.username)
-        update.message.reply_text(msg)
-    else:
-        update.message.reply_text('Not the right chat :D')
-        return
-
-def help_callback(update, context):
+def check_payment(update, context):
     if correct_chat(update.message.chat_id) is False:
-        update.message.reply_text('Not the right chat :D')
-        return
-    msg = '/help: This long text \n \
-/spent {x}: You spent x amount on your own \n \
-/paid {x}: You paid x amount to one of the others \n \
-/status: Displays the balances of each member \n\n \
-Be careful: If you paid the entire amount of a product or a service, you should use \'/spent\'. \n \
-Example usage: There are 3 members, and member 1 buys a product worth 3 dollars. \
-That member then types \'/spent 3\'. As the others pay back, they type \'/paid 1\'.'
-    update.message.reply_text(msg)
-
-def spent_callback(update, context):
-    if correct_chat(update.message.chat_id) is False:
-        update.message.reply_text('Not the right chat :D')
-        return
+        update.message.reply_text(config.wrong_chat_txt)
+        return 0
 
     try:
         amount = update.message.text.split(' ')[1]
     except:
-        update.message.reply_text('How much did you spend?')
-        return
+        update.message.reply_text(config.empty_txt)
+        return 0
 
     try:
         amount_flt = float(amount)
     except:
-        update.message.reply_text('Such a waste!')
-        return
+        update.message.reply_text(config.not_a_number_txt)
+        return 0 
 
-    if amount_flt == 0:
-        update.message.reply_text('What, really?')
-        return
+    return amount_flt
 
-    with open('balance.txt') as file:
-        balance = file.read().split(',')
-    balance_num = [0,0,0]
-    for i in range(len(balance_num)):
-        balance_num[i] = float(balance[i])
-
-    balance_before = balance
-    for i in range(len(balance_num)):
-        if i == users[str(update.message.from_user.id)]:
-            try:
-                balance_num[i] += 2.0 * amount_flt / 3.0
-            except:
-                update.message.reply_text('Nope')
-        else:
-            try:
-                balance_num[i] -= amount_flt / 3.0
-            except:
-                update.message.reply_text('Nope')
-    with open('balance.txt', 'w') as file:
-        data = ''
-        for i in range(len(balance_num)):
-            data += str(balance_num[i]) + ','
-
-        data += '\n'
-        file.write(data)
-
-    display_status(update, context, balance_before)
-
-    context.bot.send_message(chat_id=config.user1_id, text=update.message.text[7:], parse_mode='markdown')
-
-    context.bot.send_message(chat_id=config.user2_id, text=update.message.text[7:], parse_mode='markdown')
-
-    context.bot.send_message(chat_id=config.user3_id, text=update.message.text[7:], parse_mode='markdown')
-
-def paid_callback(update, context):
-
-    if correct_chat(update.message.chat_id) is False:
-        update.message.reply_text('Not the right chat :D')
-        return
-
-    try:
-        amount = update.message.text.split(' ')[1]
-    except:
-        update.message.reply_text('How much did you pay?')
-        return
-
-    try:
-        amount_flt = float(amount)
-    except:
-        update.message.reply_text('We do not accept such payments here!')
-        return
-
-    if amount_flt == 0:
-        update.message.reply_text('What, really?')
-        return
-
-    paidfrom = update.message.from_user.id
-    paidfrom = str(paidfrom)
-
+def find_payee(update,context,paidfrom):
     try:
         paidto_nickname = update.message.text.split(' ')[2]
         if paidfrom == config.user1_id:
@@ -165,9 +79,13 @@ def paid_callback(update, context):
             if paidto_nickname in config.user2_nicknames:
                 paidto = config.user2_id
     except:
-        update.message.reply_text('To whom, though?')
-        return
+        update.message.reply_text(config.paid_to_whom_text)
+        return 0
+    
+    return paidto
 
+def edit_balances(update,context,amount_flt,info):
+    
     with open('balance.txt') as file:
         balance = file.read().split(',')
 
@@ -178,12 +96,26 @@ def paid_callback(update, context):
     for i in range(len(balance_num)):
         balance_num[i] = float(balance[i])
 
-    try:
-        balance_num[users[paidfrom]] += float(amount)
-        balance_num[users[paidto]]   -= float(amount)
-    except:
-        update.message.reply_text('Nope')
-        return
+    if info[0] == 'spent':
+        for i in range(len(balance_num)):
+            if i == users[str(update.message.from_user.id)]:
+                try:
+                    balance_num[i] += 2.0 * amount_flt / 3.0
+                except:
+                    update.message.reply_text('Nope')
+            else:
+                try:
+                    balance_num[i] -= amount_flt / 3.0
+                except:
+                    update.message.reply_text('Nope')
+
+    elif info[0] == 'paid':
+        try:
+            balance_num[users[info[1]]] += amount_flt
+            balance_num[users[info[2]]] -= amount_flt
+        except:
+            update.message.reply_text('Nope')
+            return 0
 
     with open('balance.txt', 'w') as file:
         data = ''
@@ -193,11 +125,73 @@ def paid_callback(update, context):
         data += '\n'
         file.write(data)
 
+    return 1
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def start_callback(update, context):
+    if correct_chat(update.message.chat_id):
+        msg = "Hi dear master, "
+        msg += str(update.message.from_user.username)
+        update.message.reply_text(msg)
+    else:
+        update.message.reply_text(config.wrong_chat_txt)
+        return
+
+def help_callback(update, context):
+    if correct_chat(update.message.chat_id) is False:
+        update.message.reply_text(config.wrong_chat_txt)
+        return
+    update.message.reply_text(config.help_txt)
+
+def spent_callback(update, context):
+
+    amount_flt = check_payment(update, context)
+
+    if amount_flt == 0:
+        update.message.reply_text(config.zero_txt)
+        return
+
+    info = ['spent']
+
+    if edit_balances(update,context,amount_flt,info) == 0:
+        return
+
+    display_status(update, context, balance_before)
+
+    context.bot.send_message(chat_id=config.user1_id, text=update.message.text[7:], parse_mode='markdown')
+
+    context.bot.send_message(chat_id=config.user2_id, text=update.message.text[7:], parse_mode='markdown')
+
+    context.bot.send_message(chat_id=config.user3_id, text=update.message.text[7:], parse_mode='markdown')
+
+def paid_callback(update, context):
+
+    amount_flt = check_payment(update, context)
+   
+    if amount_flt == 0:
+        update.message.reply_text(config.zero_txt)
+        return
+
+    paidfrom = update.message.from_user.id
+    paidfrom = str(paidfrom)
+
+    paidto = find_payee(update,context,paidfrom)
+    if paidto == 0:
+        return
+
+    info = ['paid',paidfrom,paidto]
+
+    if edit_balances(update,context,amount_flt,info) == 0:
+        return
+
     display_status(update, context, balance_before)
 
 def status_callback(update,context):
     if correct_chat(update.message.chat_id) is False:
-        update.message.reply_text('Not the right chat :D')
+        update.message.reply_text(config.wrong_chat_txt)
         return
 
     display_status(update, context)
